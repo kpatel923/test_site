3fecb59590bf951b506e96560b346a4d6500d06c
bobdoe945 <bobdoe945@gmail.com>
2021-02-16 08:22:48 -0500

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import Post
+from .forms import ContactForm
+
+
+# Create your views here.
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog/index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+def contact_form(request):
+    form = ContactForm()
+    return render(request, 'blog/contact.html', {'form': form})
+    
